2a42384cc4db031dc22587e95c44c509c7477738
Gerry <gerrygoable@outlook.com>
2018-10-07 23:27:42 -0500

    cities

--- tour-of-databases/src/app/dashboard/dashboard.component.ts
+++ tour-of-databases/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Hero';
+import { City } from '../City';
 import { HeroService } from '../hero.service';
+
+
+interface HeroCount extends Hero {
+  saved: Number;
+}
 
 @Component({
   selector: 'app-dashboard',
@@ -8,14 +14,32 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  topHeroes: Hero[] = [];
+  topHeroes: HeroCount[] = [];
+  cities: City[];
 
   constructor(private heroService: HeroService) { }
 
-  ngOnInit() { this.getHeroes(); }
+  ngOnInit() {
+    this.getCities();
+    this.getHeroes();
+  }
 
   getHeroes () {
-    this.heroService.getHeroes().subscribe( heroes => this.topHeroes = heroes.slice(0, Math.min(heroes.length, 3)) );
+    this.heroService.getHeroes().subscribe(
+      heroes =>
+      this.topHeroes = heroes.map(
+        h => ({
+          ...h,
+          saved: this.cities.filter( c => c.hero === h.id ).length,
+        }))
+        .sort( (a, b) => (a.saved > b.saved ? -1 : b.saved > a.saved ? 1 : 0 ) )
+        // .map( pair => pair.who )
+        .slice(0, Math.min(heroes.length, 3))
+    );
+  }
+
+  getCities() {
+    this.heroService.getCities().subscribe( cit => this.cities = cit );
   }
 
 }
